effc2b5136838e430af8a815b89f25cb0674fca1
Urvancev <s.urvancev@gmail.com>
2022-12-14 12:03:31 +0300

    Update my_build.js

--- builds/my_build.js
+++ builds/my_build.js
@@ -19,7 +19,7 @@ module.exports = {
             ["HEATER_6_MAXTEMP", 350],
             ["HEATER_7_MAXTEMP", 350],
             ["BED_MAXTEMP", 135],
-            ["TEMP_SENSOR_0", 1047],
+            ["TEMP_SENSOR_0", 110],
             "PID_AUTOTUNE_MENU",
             //["MAX31865_SENSOR_OHMS_0", 100],
             //["MAX31865_CALIBRATION_OHMS_0", 430]
