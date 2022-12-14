7f5681fe478459c0c524ce6f3e1b132e9864c50f
Urvancev <s.urvancev@gmail.com>
2022-12-14 13:05:41 +0300

    Update my_build.js

--- builds/my_build.js
+++ builds/my_build.js
@@ -10,19 +10,9 @@ module.exports = {
             //standard leveling menu helper
             "LCD_BED_TRAMMING",
             "BED_TRAMMING_INCLUDE_CENTER",
-            ["HEATER_0_MAXTEMP", 350],
-            ["HEATER_1_MAXTEMP", 350],
-            ["HEATER_2_MAXTEMP", 350],
-            ["HEATER_3_MAXTEMP", 350],
-            ["HEATER_4_MAXTEMP", 350],
-            ["HEATER_5_MAXTEMP", 350],
-            ["HEATER_6_MAXTEMP", 350],
-            ["HEATER_7_MAXTEMP", 350],
+            ["HEATER_0_MAXTEMP", 330],
             ["BED_MAXTEMP", 135],
-            ["TEMP_SENSOR_0", 110],
             "PID_AUTOTUNE_MENU",
-            //["MAX31865_SENSOR_OHMS_0", 100],
-            //["MAX31865_CALIBRATION_OHMS_0", 430]
         ]
     },
     configuration_adv: {
